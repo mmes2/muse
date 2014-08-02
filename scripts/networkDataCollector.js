ea14487d2dc1469847131acb9fd45ac9736525fe
kuvelas <kuvelas@pdx.edu>
2014-08-01 22:12:19 -0700

    Everything working except alarm, which on long intervals, triggers multiple times

--- scripts/networkDataCollector.js
+++ scripts/networkDataCollector.js
@@ -25,26 +25,25 @@ var netCollect = (function() {
   
   var collector = {};
 
-  collector.currentSent = null;
-  collector.currentReceived = null;
- 
-
-  collector.oldDataSent = null;
-  collector.oldDataReceived = null;
-
-
-  collector.DataReceived = null;
-  collector.DataSent = null;
-  collector.Name = null;
-  collector.ConnectionType = null;
-  collector.Name = null;
-  collector.Bandwidth = null;
-  collector.SignalStrength = null;
-  collector.mobileSignalStrength = null;
-  collector.mobileNetwork = null;
-  collector.mobileData = null;
-  collector.Latitude = false;
-  collector.Longitude = false;
+  collector.StartTime = new Date();
+  collector.EndTime = collector.StartTime;
+  collector.Latitude = null;
+  collector.Longitude = null;
+  
+  collector.WifiDataReceived = null;
+  collector.WifiDataSent = null;
+  collector.WifiNetwork = null;
+  collector.WifiData = null;
+  collector.WifiBandwidth = null;
+  collector.WifiSignalStrength = null;
+  
+  collector.MobileSignalStrength = null;
+  collector.MobileNetwork = null;
+  collector.MobileData = null;
+  collector.MobileRoaming = null;
+  
+  
+  
  
   collector.activate = function (PriorityLevel) {
 
@@ -53,42 +52,43 @@ var netCollect = (function() {
 
     var oneMinute = 60000; // ms
     if(PriorityLevel==1) { 
-      //rate = 300000; 
+      rate = 300000; 
       minutes = 5;    
     } else if(PriorityLevel==2) { 
-      //rate = 600000; 
+      rate = 600000; 
       minutes = 10;   
     } else if(PriorityLevel==3) { 
-      //rate = 900000; 
+      rate = 900000; 
       minutes = 15;
     } else if(PriorityLevel==4) { 
-      //rate = 1800000;  
+      rate = 1800000;  
       minutes = 30; 
     } else { 
-      //rate = 3600000; 
+      rate = 3600000; 
       minutes = 60;
     } 
 
     var d = new Date();
     var alarmId;
-    var timeout = 10000 //(minutes - d.getMinutes() % minutes) * oneMinute - (d.getSeconds() * 1000);
+    var timeout = (minutes - d.getMinutes() % minutes) * oneMinute - (d.getSeconds() * 1000);
     d.setTime(d.getTime() + timeout); 
 
     var collectAlarm = navigator.mozAlarms.add(d, "ignoreTimezone", {});
 
 
     collectAlarm.onsuccess = function () {
-      console.log("alarm was scheduled for: " + d.toString());
-          alarmId = this.result;
+      collector.StartTime = new Date(); 
+      console.log("Starting collection at: " + collector.StartTime);
+      alarmId = this.result;
     };
 
     collectAlarm.onerror = function () {
       console.log("Unable to schedule alarm: " + this.error.name);
     };
     navigator.mozSetMessageHandler("alarm", function (mozAlarm) { 
-      var d2 = new Date(); 
-      console.log("Collection alarm fired at: " + d2.toString()); 
-
+      collector.EndTime = new Date(); 
+      console.log("Ending collection at: " + collector.EndTime); 
+      
       if (alarmId) {
         navigator.mozAlarms.remove(alarmId);
       }
@@ -112,11 +112,17 @@ var netCollect = (function() {
       if(navigator.mozWifiManager.connection){
       
         if(navigator.mozWifiManager.connection.status === "connected"){
-          collector.ConnectionType = "wifi";
-          collector.Name = navigator.mozWifiManager.connection.network.ssid;
-          collector.Bandwidth = navigator.mozWifiManager.connectionInformation.linkSpeed;  //in Mb/s
-          collector.SignalStrength = navigator.mozWifiManager.connectionInformation.relSignalStrength;
+          collector.WifiData = true;
+          collector.WifiNetwork = navigator.mozWifiManager.connection.network.ssid;
+          collector.WifiBandwidth = navigator.mozWifiManager.connectionInformation.linkSpeed;  //in Mb/s
+          collector.WifiSignalStrength = navigator.mozWifiManager.connectionInformation.relSignalStrength;
+        }else{
+          collector.WifiData = false;
+          collector.WifiNetwork = null;
+          collector.WifiBandwidth = null;
+          collector.WifiSignalStrength = null;
         }
+        
       }
     }else {
       console.log("no mozWifiManager");
@@ -133,134 +139,70 @@ var netCollect = (function() {
         var mData = tempMobile[0].data;
         
         if(mData.connected === true){
-          collector.mobileData = true;
-          collector.mobileNetwork = mData.network;
-          collector.mobileSignalStrength = mData.signalStrength;          
-          collector.mobileBandwidth = "unknown";
+          collector.MobileData = true;
+          collector.MobileNetwork = mData.network;
+          collector.MobileSignalStrength = mData.relSignalStrength;          
+          collector.MobileBandwidth = "unknown";
+          collector.MobileRoaming = mData.roaming;
+          collector.MobileMetered = true;
+        }else{
+          collector.MobileData = false;
+          collector.MobileNetwork = null;
+          collector.MobileSignalStrength = null;          
+          collector.MobileBandwidth = null;
+          collector.MobileRoaming = null;
+          collector.MobileMetered = null;
         }  
     }else {
       console.log("no mozMobileConnections");
     }
 
     
-    var Metered = null;
-    /*Need a replacement for navigator.connection.. It holds no useful data
-       */
-    if(navigator.connection) {
-      var connection = navigator.connection || navigator.webkitConnection;
-      Metered = connection.metered;
-    }
-
-
-    //GPS part. This might give bad data if geolocation not allowed by user
-    var geoTest = navigator.geolocation;
-    
-    if (navigator.geolocation) {
-
-      var success = function (place) {
-        collector.Latitude = place.coords.latitude;
-        collector.Longitude = place.coords.longitude;
-      };
+    collector.setGeolocation(function () {
+          
+      collector.setSentRecieved(function () {
       
-      var error = function (place) {
-        console.log("Couldn't get gps position");
-      };
+          collector.collectNetworkStats.writeRecord();
+          callback();
+
+      });
       
-      navigator.geolocation.getCurrentPosition(success,error);
-            
-    }else { 
-      console.out("no geolocation");
-    }
-
-    
-    
-    
-    
-    
-    //delete this, its for testing
-    
-      var end = new Date();    
-      var start = new Date(end-90000);
-    
-      var netStats = navigator.mozNetworkStats;
-      var networks = netStats.getAvailableNetworks();  
-
-      networks.onsuccess = function () {
-        var aNetwork = networks.result[0];
-
-        var receive = 0;
-        var send = 0;
-        
-        var netSamples = netStats.getSamples(aNetwork, start, end); 
-
-        
-
-        
-        netSamples.onsuccess = function () {
-
-          
-          
-          var rData = netSamples.result.data;
-          
-          rData.forEach(function (chunk) {
-            receive += chunk.rxBytes;
-            send    += chunk.txBytes;
-            console.log("**Rec: " + receive);
-            console.log("**Sent: " + send);
-          });
-          
-          
-          
-        };
-        
-      }; 
-
-    /////////////////////////
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //Getting data sent and received
-    collector.getSentRecieved(function () {
-      collector.DataReceived = collector.currentReceived - collector.oldDataReceived;
-      collector.oldDataReceived = collector.currentReceived;
-      collector.DataSent = collector.currentSent - collector.oldDataSent;
-      collector.oldDataSent = collector.currentSent;    
-    
-      setTimeout(function () {
-        collector.collectNetworkStats.writeRecord();
-        callback();
-      }, 10000);
-  
     });
+    
 
+///////////////collectNetworkStats Helper functions/////////////////
 
-///////////////Helper functions/////////////////
-
+    
     collector.collectNetworkStats.writeRecord = function() {
 
       collector.collectNetworkStats.printLogs();
 
       netStatsDB.addRecord( 
-      { "Interval":Interval, 
+      { 
+        "Start": collector.StartTime,
+        "End": collector.EndTime,
         "CollectionDate":d,
-        "Name":collector.Name, 
-        "ConnectionType":collector.ConnectionType, 
-        "Metered":Metered,
-        "Bandwidth":collector.Bandwidth, 
-        "SignalStrength":collector.SignalStrength,
-        "DataReceived":collector.DataReceived,
-        "DataSent":collector.DataSent,
         "Latitude":collector.Latitude,
-        "Longitude":collector.Longitude
+        "Longitude":collector.Longitude,
+        "Wifi":{
+          "Connected":collector.WifiData,
+          "NetworkName":collector.WifiNetwork, 
+          "Bandwidth":collector.WifiBandwidth, 
+          "SignalStrength":collector.WifiSignalStrength,
+          "DataReceived":collector.WifiDataReceived,
+          "DataSent":collector.WifiDataSent        
+        },
+        Mobile:{
+          "Connected":collector.MobileData,
+          "NetworkName":collector.MobileNetwork,
+          "Bandwidth":collector.MobileBandwidth,
+          "SignalStrength":collector.MobileSignalStrength,
+          "DataReceived":0,
+          "DataSent":0,
+          "Roaming":collector.MobileRoaming,
+          "Metered":collector.MobileMetered          
+        }
+      
       });
       
       //callback();
@@ -269,25 +211,52 @@ var netCollect = (function() {
 
     collector.collectNetworkStats.printLogs = function () {
       console.log("****Collection Record****");
-      console.log("Network Name: " + collector.Name);
-      console.log("Connection Type: " + collector.ConnectionType);
-      console.log("Metered: " +  Metered);
-      console.log("Bandwidth: "+ collector.Bandwidth + " Mb/s");  
-      console.log("SignalStrength: " + collector.SignalStrength + "%");
-      console.log("DataReceived: "+ collector.DataReceived + " bytes");
-      console.log("DataSent: " + collector.DataSent + " bytes");
+      console.log("Wifi Network Name: " + collector.WifiNetwork);
+      
+      
+      console.log("Wifi Bandwidth: "+ collector.WifiBandwidth + " Mb/s");  
+      console.log("Wifi SignalStrength: " + collector.WifiSignalStrength + "%");
+      console.log("Wifi DataReceived: "+ collector.WifiDataReceived + " bytes");
+      console.log("Wifi DataSent: " + collector.WifiDataSent + " bytes");
       console.log("Latitude: "+ collector.Latitude);
       console.log("Longitude: " + collector.Longitude);
       console.log("****End of Record****");
     };
   };
 
-  collector.getSentRecieved = function (callback) {
+  
+///////////////collector Helper functions/////////////////
+
+  collector.setGeolocation = function(callback) {
+  
+    var geoTest = navigator.geolocation;
+    
+    if (navigator.geolocation) {
+
+      var success = function (place) {
+        collector.Latitude = place.coords.latitude;
+        collector.Longitude = place.coords.longitude;
+        callback();       
+      };
+      
+      var error = function (place) {
+        console.log("GPS denied by user");
+        callback();
+      };
+      
+      navigator.geolocation.getCurrentPosition(success,error);
+            
+    }else { 
+      console.log("no geolocation");
+      callback();
+    }
+    
+    
+  };
+  
+  collector.setSentRecieved = function (callback) {
  
     if(navigator.mozNetworkStats){
-
-      var end = new Date();    
-      var start = new Date(end);
 
       var netStats = navigator.mozNetworkStats;
       var networks = netStats.getAvailableNetworks();  
@@ -295,24 +264,27 @@ var netCollect = (function() {
       networks.onsuccess = function () {
         var aNetwork = networks.result[0];
 
-        var netSamples = netStats.getSamples(aNetwork, start, end); 
+        var netSamples = netStats.getSamples(aNetwork, collector.StartTime, collector.EndTime); 
 
         netSamples.onsuccess = function () {
           var rData = netSamples.result.data;
-          //console.log("**Rec: " + rData[0].rxBytes);
-          //console.log("**Sent: " + rData[0].txBytes);
-          collector.currentReceived = rData[0].rxBytes;
-          collector.currentSent = rData[0].txBytes; 
+          
+          collector.WifiDataReceived = rData[0].rxBytes;
+          collector.WifiDataSent = rData[0].txBytes; 
+          
+          setTimeout(function () {
+            netStats.clearAllStats(); 
+            callback();}
+                     ,1000);       
         };
         
       }; 
-     setTimeout(callback(),1000);       
     }
   };
 
-  collector.getSentRecieved(function () {
-      collector.oldDataReceived = collector.currentReceived;
-      collector.oldDataSent = collector.currentSent;      
+  ///////////////Clear rx/tx values before first collection/////////////////
+  collector.setSentRecieved(function () {
+    console.log("cleared sent/received data");
   });
 
   return collector;
